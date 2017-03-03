7079a5bac9b5804f58bfaccb1113681f3503cd9f
John Olivo <jolivo2@bsbason.edu>
2017-03-02 20:20:54 -0500

    made minor corrections

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class BlogHandler(webapp2.RequestHandler):
             Get all posts by a specific user, ordered by creation date (descending).
             The user parameter will be a User object.
         """
-        user_query = Post.all().filter('author', author)
+        user_query = Post.all().order('-created').filter('author', author)
         return user_query.fetch(limit=limit, offset=offset)
         # TODO - filter the query so that only posts by the given user
         return None
